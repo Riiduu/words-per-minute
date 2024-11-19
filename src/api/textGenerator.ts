615932df5ec68061c474d8348879be0c50f6add3
Soapman <jelinsksk@gmail.com>
2024-11-19 12:53:01 +0200

    turned into a function

--- src/api/textGenerator.ts
+++ src/api/textGenerator.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const randomText =
-    axios.get('https://fakerapi.it/api/v2/texts?_quantity=1&_characters=350')
-        .then(function (res) {
-            if (res != null) {
-                return res.data.data[0].content;
-            }
-        })
+export const randomText = async () => {
+    const res = await axios.get('https://fakerapi.it/api/v2/texts?_quantity=1&_characters=350');
+    if (res?.data?.data?.[0]?.content) {
+        return res.data.data[0].content;
+    }
+    return ""; // Fallback in case of error or no data
+};
